2a3fb749265d895503412b40117deeed38411c71
thomasfedb <thomas@picent.com.au>
2010-08-01 19:45:38 +0800

    init in script

--- set_in.rb
+++ set_in.rb
@@ -1,4 +1,7 @@
 $: << File.expand_path(File.dirname(__FILE__) + "/includes")
 require "servo_controller/init.rb"
 
-@interfaces
+@interfaces.interfaces.each do |interface|
+  interface.init_servos(1, 64)
+  interface.set_servos(1, 2000, 64)
+end
